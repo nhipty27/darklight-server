71582d1468624da4c93d252b48641e60ff2b1677
20120541 <122914670+ptynhi9102@users.noreply.github.com>
2023-02-10 16:10:08 +0700

    fix bug

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.use("/comment", commentRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000
+mongoose.set("strictQuery", false)
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
